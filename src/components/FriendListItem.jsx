88b6a54a70eeedf2bae520911ec180a8ac9d6fdb
klaudiaszpigun <141838129+klaudiaszpigun@users.noreply.github.com>
2024-04-04 18:14:47 +0200

    .

--- src/components/FriendListItem.jsx
+++ src/components/FriendListItem.jsx
@@ -1,8 +1,9 @@
+import '../css/friendlist.css';
 export const FriendListItem = ({ friendsArray }) => {
   return friendsArray.map(friend => {
     return (
       <li>
-        <span>.</span>
+        <span class={friend.isOnline ? 'online' : 'offline'}>.</span>
         <img src={friend.avatar} alt="user avatar" />
         <p>{friend.name}</p>
       </li>
