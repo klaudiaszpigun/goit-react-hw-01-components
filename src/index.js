69f06a6229fe608e035074195627a970e820fef7
klaudiaszpigun <141838129+klaudiaszpigun@users.noreply.github.com>
2024-04-04 16:18:55 +0200

    .

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './components/App';
-import './index.css';
+
+import data from '../src/data/data.json';
+import friends from '../src/data/friends.json';
+import items from '../src/data/transactions.json';
+import user from '../src/data/user.json';
+
+import { FriendListItem } from 'components/FriendListItem';
+import { FriendList } from './FriendList';
+import { Profile } from './Profile';
+import { Statistics } from './Statistics';
+import { TransactionHistory } from './TransactionHistory';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <App>
+    <Profile user={user} />
+    <Statistics data={data} />
+    <FriendList friends={friends}>
+      <FriendListItem />
+    </FriendList>
+    <TransactionHistory items={items} />
+  </App>
 );
